c87e7b763fadf08c2c7af605e704e7d61c74ccd8
Kim H <kthoalim@gmail.com>
2020-06-08 17:41:08 -0400

    Added comments section

--- WWCI CSharp Loops/Program.cs
+++ WWCI CSharp Loops/Program.cs
@@ -6,7 +6,15 @@ namespace WWCI_CSharp_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Aprogram that loops over a number and prints it
+             * Let's create a quick algorithm / pseudocode
+             * 
+             * 1) Identify number of times we'll be looping
+             * this will be the condition of our loop statement, meaning if the number we're on is less than or equal to this number, we'll print it.
+             * 2) We'll need a variable to hold the number we're counting. We'll then increment that number after every loop
+             * 3) Inside the loop, write out the number to the screen.             
+            */
         }
     }
 }
